05545afa71b94251ad7aaaa834833b9061acf8d7
Aykın Sancaklı <sancakliaykin@gmail.com>
2024-10-08 02:55:42 +0300

    Complete initial setup and connect DB

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+// MIDDLEWARES
+app.use(express.json());
+
+// ROUTES
+// DEFINE ROUTES HERE OR IMPORT FROM A SEPERATE ROUTES FILE
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,32 @@
-const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = require("./app");
 const cors = require("cors");
+
+dotenv.config({ path: "./config.env" });
+
 const corsOptions = {
   origin: ["http://localhost:5173"],
 };
-
 app.use(cors(corsOptions));
 
+// Connect to MongoDB
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB)
+  .then(() => console.log("DB Connection successful!"))
+  .catch((err) => console.error("DB connection error:", err));
+
+// API Endpoint
 app.get("/api", (req, res) => {
   res.json({ places: ["trevi", "madrid", "rome"] });
 });
 
-app.listen(8080, () => {
-  console.log("Server started on port 8080");
+// Start the server
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
 });
