9ccd828e84ac3f21add9e5e7b2adc5777c46ae18
Aykın Sancaklı <sancakliaykin@gmail.com>
2024-10-15 02:37:53 +0300

    Create page navigation component

---
+++ client/src/components/PageNav/PageNav.jsx
@@ -0,0 +1,29 @@
+import { NavLink } from "react-router-dom";
+import Logo from "../Logo/Logo";
+import styles from "./PageNav.module.scss";
+
+function PageNav() {
+  return (
+    <div>
+      <nav className={styles.nav}>
+        <Logo />
+
+        <ul>
+          <li>
+            <NavLink to="/pricing">Pricing</NavLink>
+          </li>
+          <li>
+            <NavLink to="/product">Product</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login" className={styles.ctaLink}>
+              Login
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default PageNav;
