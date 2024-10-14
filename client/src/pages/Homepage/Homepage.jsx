fd9fe439473f37b7b59b379d736997592f962243
Aykın Sancaklı <sancakliaykin@gmail.com>
2024-10-15 02:34:34 +0300

    Create homepage

--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -1,5 +1,29 @@
+import { Link } from "react-router-dom";
+import styles from "./Homepage.module.scss";
+import PageNav from "../../components/PageNav/PageNav";
+
 function Homepage() {
-  return <div></div>;
+  return (
+    <main className={styles.homepage}>
+      <PageNav />
+
+      <section>
+        <h1>
+          Every journey has a story.
+          <br />
+          TripTracks captures all its glory.
+        </h1>
+        <h2>
+          A world map that turns every place you visit into a piece of your
+          story. Mark the world with your travels and revisit the moments that
+          shaped your adventures.
+        </h2>
+        <Link to="/login" className={styles.cta}>
+          Start tracking now
+        </Link>
+      </section>
+    </main>
+  );
 }
 
 export default Homepage;
