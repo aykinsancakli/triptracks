bf18d0ef04d8f31ea72475c270facdd46506a443
Aykın Sancaklı <sancakliaykin@gmail.com>
2024-10-15 02:37:34 +0300

    Create Product page

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,5 +1,32 @@
+import PageNav from "../../components/PageNav/PageNav";
+import styles from "./Product.module.scss";
+import img from "../../assets/img-1.jpg";
+
 function Product() {
-  return <div></div>;
+  return (
+    <main className={styles.product}>
+      <PageNav />
+
+      <section>
+        <img src={img} alt="person with dog overlooking mountain with sunset" />
+        <div>
+          <h2>About TripTracks</h2>
+          <p>
+            TripTracks is your personal travel companion, built to preserve the
+            memories of every journey you take. Whether you’re wandering through
+            bustling cities or exploring hidden corners of the world, TripTracks
+            ensures that no moment goes unnoticed.
+          </p>
+          <p>
+            With TripTracks, every place you visit is saved, every memory
+            cherished. Our seamless interface allows you to track your travels
+            effortlessly, giving you a beautiful visual record of your global
+            adventures.
+          </p>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default Product;
