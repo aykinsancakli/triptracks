421e4eb7bc3b850dce40fa72260806465c8b3aa8
Aykın Sancaklı <sancakliaykin@gmail.com>
2024-10-16 23:11:14 +0300

    Create user model

---
+++ server/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Please enter a valid email"],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, "Please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter a password"],
+    minlength: [6, "Minimum password length is 6 characters"],
+  },
+});
+
+// Hash password before saving the user
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
