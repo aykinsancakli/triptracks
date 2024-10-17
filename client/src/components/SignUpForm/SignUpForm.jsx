7d19fea9044883417fe35cab03c312bc7cdaef33
Aykın Sancaklı <sancakliaykin@gmail.com>
2024-10-17 13:45:57 +0300

    Complete sign up form

--- client/src/components/SignUpForm/SignUpForm.jsx
+++ client/src/components/SignUpForm/SignUpForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { Link } from "react-router-dom"; // Import Link for navigation
+import { Link, useNavigate } from "react-router-dom";
 import styles from "../LoginForm/LoginForm.module.scss";
+import Spinner from "../Spinner/Spinner";
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
@@ -9,12 +10,14 @@ function SignUpForm() {
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({ email: "", password: "" });
   const [successMessage, setSuccessMessage] = useState("");
-  const [accountCreated, setAccountCreated] = useState(false); // Track account creation status
+  const [accountCreated, setAccountCreated] = useState(false);
+
+  const navigate = useNavigate();
 
   async function handleSubmit(e) {
     e.preventDefault();
     setErrors({ email: "", password: "" });
-    setSuccessMessage(""); // Reset success message
+    setSuccessMessage("");
 
     try {
       const res = await fetch(`${BASE_URL}/signup`, {
@@ -32,14 +35,14 @@ function SignUpForm() {
       }
 
       if (data.user) {
-        // Show success message and update account creation status
         setSuccessMessage("Account successfully created!");
-        setAccountCreated(true); // Set account created to true
+        setAccountCreated(true);
 
         // Hide success message after 4 seconds
         setTimeout(() => {
           setSuccessMessage("");
-        }, 4000);
+          navigate("/app");
+        }, 2000);
       }
     } catch (err) {
       console.log(err);
@@ -49,50 +52,51 @@ function SignUpForm() {
   return (
     <div>
       <form className={styles.form} onSubmit={handleSubmit}>
-        <div className={styles.heading}>
-          <h2>Create your account</h2>
-          <p>
-            Already have an account? <Link to="/login">Log in</Link>
-          </p>
-        </div>
-        <div className={styles.row}>
-          <label htmlFor="email">Email address</label>
-          <input
-            type="text"
-            id="email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            placeholder="Email"
-          />
-          <span className={styles.error}>
-            {errors.email && <p>{errors.email}</p>}
-          </span>
-        </div>
+        {!accountCreated ? (
+          <>
+            <div className={styles.heading}>
+              <h2>Create your account</h2>
+              <p>
+                Already have an account? <Link to="/login">Log in</Link>
+              </p>
+            </div>
+            <div className={styles.row}>
+              <label htmlFor="email">Email address</label>
+              <input
+                type="text"
+                id="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                placeholder="Email"
+              />
+              <span className={styles.error}>
+                {errors.email && <p>{errors.email}</p>}
+              </span>
+            </div>
 
-        <div className={styles.row}>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            placeholder="Password"
-          />
-          <span className={styles.error}>
-            {errors.password && <p>{errors.password}</p>}
-          </span>
-        </div>
+            <div className={styles.row}>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                placeholder="Password"
+              />
+              <span className={styles.error}>
+                {errors.password && <p>{errors.password}</p>}
+              </span>
+            </div>
 
-        <div className={styles.buttons}>
-          <button className={styles.btn}>Sign Up</button>
-
-          {/* Show this button permanently if the account is created successfully */}
-          {accountCreated && (
-            <Link className={styles.btnLink} to="/login">
-              <button className={styles.loginBtn}>&larr; Login</button>
-            </Link>
-          )}
-        </div>
+            <div>
+              <button className={styles.btn}>Sign Up</button>
+            </div>
+          </>
+        ) : (
+          <div className={styles.redirect}>
+            <Spinner message="Hang tight! We're setting up your account..." />
+          </div>
+        )}
       </form>
 
       {successMessage && (
