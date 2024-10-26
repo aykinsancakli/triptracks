3a934336c23d6c50f5dd314825c4db60d290d116
Aykın Sancaklı <sancakliaykin@gmail.com>
2024-10-26 22:00:43 +0300

    Add overlay to display a message for lower screen sizes

--- client/src/pages/AppLayout/AppLayout.jsx
+++ client/src/pages/AppLayout/AppLayout.jsx
@@ -7,6 +7,8 @@ import Main from "../../components/Main/Main";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import MapView from "../../components/MapView/MapView";
 import User from "../../components/User/User";
+import Logo from "../../components/Logo/Logo";
+import { Link } from "react-router-dom";
 
 function AppLayout() {
   // Load maps script => (need after this point since we are in the app)
@@ -17,14 +19,26 @@ function AppLayout() {
 
   return (
     isLoaded && (
-      <div className={styles.app}>
-        <Nav />
-        <Main>
-          <Sidebar />
-          <MapView />
-        </Main>
-        <User />
-      </div>
+      <>
+        <div className={styles.app}>
+          <Nav />
+          <Main>
+            <Sidebar />
+            <MapView />
+          </Main>
+          <User />
+        </div>
+        <div className={styles.overlay}>
+          <Logo />
+          <h2>
+            For the best experience, please use TripTracks on a tablet or larger
+            screen.
+          </h2>
+          <Link to="/" className={styles.link}>
+            Go back to homepage
+          </Link>
+        </div>
+      </>
     )
   );
 }
